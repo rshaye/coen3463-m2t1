cb8d0ff141e026cb560e43d9fa1b9d0247cc3fc0
Romina <shaye.romina@yahoo.com>
2017-01-07 15:24:42 +0800

    Initial Commit

--- routes/contact.js
+++ routes/contact.js
@@ -17,13 +17,13 @@ router.post('/send', function(req, res, next){
 	});
 
 	var mailOptions = {
-		from: 'name <coen3463t15@gmail.com>',
-		to: 'coen3463t15@gmail.com',
+		from: 'name <teamjuan115@gmail.com>',
+		to: 'teamjuan115@gmail.com',
 		subject: 'inquiries',
 		text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.Message,
 		html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Message: '+req.body.message+ '</li></ul>'
 	};
-	
+
 	trasporter.sendMail(mailOptions, function(error, info){
 		if (error){
 			console.log(error);
